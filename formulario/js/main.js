d773245c1953a504e1b47b52bce4fae610ed3bae
juancamargo01 <74567426+juancamargo01@users.noreply.github.com>
2021-08-01 16:24:52 -0300

    finalizado validação
    finalizado validação do front-end

--- formulario/js/main.js
+++ formulario/js/main.js
@@ -13,6 +13,33 @@ class ValidaFormulario{
     e.preventDefault();
     console.log('formulario nao enviado');
     const camposValidos = this.camposSaoValidos();
+    const senhasVAlidas = this.senhasSaoValidas();
+
+    if (camposValidos && senhasVAlidas){
+      alert('Formulario enviado');
+      this.formulario.submit();
+    }
+  }
+  senhasSaoValidas(){
+    let valid = true;
+
+    const senha = this.formulario.querySelectorAll('.senha');
+    const repetirsenha = this.formulario.querySelectorAll('.repetir-senha');
+
+    if(senha.value !== repetirsenha.value){
+      valid = false;
+
+      this.criaErro(senha,'Compos senha e repetir senha precisar ser iguais.');
+      this.criaErro(repetirsenha,'Compos senha e repetir senha precisar ser iguais.');
+    }
+
+    if(senha.value.length < 6 || senha.value.value > 12){
+      valid = false;
+
+      this.criaErro(senha,'Senha precisa conter entre 6 e 12 caracteres.');
+    }
+
+    return valid;
   }
 
   camposSaoValidos(){
@@ -24,14 +51,34 @@ class ValidaFormulario{
     for (let campo of this.formulario.querySelectorAll('.validar')){
       const label = campo.previousElementSibling.innerHTML;
       if(!campo.value){
-        this.criaErro(campo,`Campo "${label} nao pode estar em branco"`)
+        this.criaErro(campo,`Campo "${label} nao pode estar em branco."`)
         valid = false;
       }
 
       if (campo.classList.contains('cpf')){
         if(!this.validaCPF(campo)) valid =false;
       }
+
+      if (campo.classList.contains('usuario')){
+        if(!this.validaUsuario(campo)) valid =false;
+      }
     }
+    return valid;
+  }
+
+  validaUsuario(campo){
+    const usuario = campo.value;
+    let valid = true;
+    if(usuario.length >12 || usuario.length<3){
+      this.criaErro(campo,'usuario precisa ter entre 3 e 12 caracteres.');
+      valid = false;
+    }
+    if(!usuario.match(/^[a-zA-Z0-9]+$/g)){
+      this.criaErro(campo,'Nome de usuario precisa conter apenas letras e/ou números.');
+      valid = false;
+    }
+
+    return valid;
   }
 
   validaCPF(campo){
